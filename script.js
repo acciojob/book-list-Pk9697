922df8758b9ca5f6d8fa69db307f8fe7a0aed3d1
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-05-29 14:13:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+const titleInput=document.getElementById('title')
+const authorInput=document.getElementById('author')
+const isbnInput=document.getElementById('isbn')
+const submitButton=document.getElementById('submit')
+const tbody=document.getElementById('book-list')
+
+let booksList=[]
+
+submitButton.addEventListener('click',()=>{
+	booksList.unshift({
+			id:booksList.length,
+			title:titleInput.value,
+			author:authorInput.value,
+			isbn:isbnInput.value
+		})
+
+	render()
+})
+
+function handleDelete(id) {
+	booksList=booksList.filter(book=>book.id!=id)
+	render();
+}
+
+function render(){
+	tbody.innerHTML=booksList.map(book=>`
+		<tr>
+			<td>${book.title}</td>
+			<td>${book.author}</td>
+			<td>${book.isbn}</th>
+			<td>
+				<button class="delete" id=${book.id} onclick="handleDelete(${book.id})">Delete</button>
+			</td>
+		</tr>
+	`).join(' ')
+}
+
+
+
+
+
+
